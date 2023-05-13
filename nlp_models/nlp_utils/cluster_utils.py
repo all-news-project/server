c360ce89dfb1039578cecf5c390874489a5e8a00
TalSomech <talsomech@gmail.com>
2023-05-13 21:40:14 +0300

    deleted unnecessary test and models and change cluster similarity function

--- nlp_models/nlp_utils/cluster_utils.py
+++ nlp_models/nlp_utils/cluster_utils.py
@@ -1,4 +1,6 @@
 from db_driver import get_current_db_driver
+from db_driver.db_objects.article import Article
+from db_driver.db_objects.cluster import Cluster
 from logger import get_current_logger, log_function
 from nlp_models.exceptions import SimilarityNotFoundException
 from nlp_models.nlp_utils.nlp_utils import compare_text
@@ -17,19 +19,25 @@ def cluster_similarity(cluster_id: str, new_article_id: str) -> float:
     sim_rate = compare_text([main_article.article_content, new_article.content])
 
     if sim_rate > 60:
-        avg_rate = 0
-        articles = db.get_many("articles", {"article_id": cluster.articles_id})
-        for article in articles:
-            avg_rate = avg_rate + compare_text([new_article, article.summary])
-        avg_rate = avg_rate / len(articles)
+        avg_rate = _check_avg_cluster_sim(cluster, new_article)
         if sim_rate > 75 and avg_rate > 60:
             logger.debug(f"High similarity with article,average{avg_rate}")
         elif sim_rate > 60 and avg_rate > 70:
             logger.debug(f"Low similarity with article,average{avg_rate}")
-        final_result=(avg_rate + sim_rate) / 2
+        final_result = (avg_rate + sim_rate) / 2
         logger.debug(f"similarity rate to cluster is {final_result}")
         return final_result
     else:
         desc = f"Error Similarity rate {sim_rate}, Similarity not found"
         logger.debug(f"Similarity not found in {cluster.cluster_id}")
         raise SimilarityNotFoundException(desc)
+
+
+@log_function
+def _check_avg_cluster_sim(cluster: Cluster, new_article: Article):
+    db = get_current_db_driver()
+    avg_rate=0
+    articles = db.get_many("articles", {"article_id": cluster.articles_id})
+    for article in articles:
+        avg_rate = avg_rate + compare_text([new_article, article.summary])
+    avg_rate = avg_rate / len(articles)
