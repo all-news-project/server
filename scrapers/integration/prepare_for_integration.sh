9627c4dde64ec38c8fe786cfa3bc0d58da430114
kggold4 <kggold4@gmail.com>
2023-07-31 23:01:31 +0300

    Fix scrapers integration

--- scrapers/integration/prepare_for_integration.sh
+++ scrapers/integration/prepare_for_integration.sh
@@ -1,4 +1,14 @@
-rf -r scapres
-mkdir scapres
-cp ../* scapres
-cp ../../server_utils scapres
+# Build folder
+rm -r scrapers_build
+mkdir scrapers_build
+
+# Scrapers
+mkdir scrapers_build/scrapers
+cp -r ../scraper_drivers scrapers_build/scrapers
+cp -r ../websites_scrapers scrapers_build/scrapers
+cp ../__init__.py scrapers_build/scrapers
+cp ../logic_scraper.py scrapers_build/scrapers
+cp ../run.py scrapers_build/scrapers
+
+# Utils
+cp -r ../../../server_utils/* scrapers_build/scrapers
